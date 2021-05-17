ac0ca0812c3353aecc2beda5e2975b75a9121862
ngianluca <gianluca.notaro@exense.ch>
2021-05-17 16:44:30 +0200

    dfsdf

--- test.py
+++ test.py
@@ -19,8 +19,34 @@ options = PipelineOptions(
 # with beam.Pipeline(options=options) as pipeline:
 #   pass  # build your pipeline here.
 
-def run():
-    p = Pipeline(options=options)
+def run(argv=None, save_main_session=True):
+  """Build and run the pipeline."""
+  
+  pipeline_options = PipelineOptions(save_main_session=True, streaming=True)
+  
+  with beam.Pipeline(options=pipeline_options) as p:
+      
+    input_subscription=f"projects/{PROJECT}/subscriptions/{SUBSCRIPTION}"
+    output_table=f"{PROJECT}:{DATASET}.{TABLE}"
+    
+    _ = (
+        
+        p
+        | 'Read from Pub/Sub' >> beam.io.ReadFromPubSub(subscription=input_subscription).with_output_types(bytes)
+        | 'UTF-8 bytes to string' >> beam.Map(lambda msg: msg.decode('utf-8'))
+        #| beam.Map(print)
+        | beam.Map(create_random_record)
+        | 'Write to Table' >> beam.io.WriteToBigQuery(
+                        output_table,
+                        schema = SCHEMA,
+                        custom_gcs_temp_location=BUCKET,
+                        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                        write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+                        )
+        
+        )
 
-if __name__ == "__main__":
-    run()
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
+
